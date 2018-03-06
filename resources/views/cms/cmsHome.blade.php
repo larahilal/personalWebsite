4b96c5356f967bb633c26d617119c99126ed3702
Lara Hilal <lara.mustardino@gmail.com>
2018-03-06 14:27:13 -0800

    Added Nav Bar to every page in the CMS

--- resources/views/cms/cmsHome.blade.php
+++ resources/views/cms/cmsHome.blade.php
@@ -2,15 +2,6 @@
 
 @section('content')
 
-    <a href="{{ route('logoForm') }}">Add a logo</a>
-
-    <a href="{{ route('searchForm') }}">Search</a>
-
-    <a href="{{ route('newArticleForm') }}">Create</a>
-
-    <a href="{{ route('allArticles') }}">View</a>
-
-    <a href="{{ route('logout') }}">Logout</a>
 
 
 @stop
