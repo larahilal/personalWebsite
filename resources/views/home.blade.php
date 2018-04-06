4999580a5525c6a8182d7aa2cad633e5c1aaccab
Lara Hilal <lara.mustardino@gmail.com>
2018-04-06 12:56:09 -0700

    Fixed plural logic on Comment count (1 comment 2 comment-s)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,9 +23,11 @@
 
         <h4>{!! $article->abbreviation !!}</h4>
 
-
-        <h4>{{ $article->comments->count() }} Comments</h4>
-
+        @if($article->comments->count()<2 and $article->comments->count()>0)
+        <h4>{{ $article->comments->count() }} Comment</h4>
+        @else
+            <h4>{{ $article->comments->count() }} Comments</h4>
+        @endif
         <a href="{{ route('displayFullArticle', array('articleId' => $article->id)) }}">...read more</a>
 
         <hr />
